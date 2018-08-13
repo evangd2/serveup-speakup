28c9bfcfe56a06d1a7cc046ed93062f84075fc5a
kckc1289 <keziah.camille8@ymail.com>
2018-08-13 11:54:13 -0700

    Fixed app.yaml configuration issues

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ jinja_env = jinja2.Environment(
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        welcome_template = jinja_env.get_template("templates/welcome.html")
+        welcome_template = jinja_env.get_template("/templates/welcome.html")
         self.response.write(welcome_template.render())
 
 class NewsHandler(webapp2.RequestHandler):
     def get(self):
-        news_template = jinja_env.get_template("templates/news.html")
+        news_template = jinja_env.get_template("/templates/news.html")
         self.response.write(news_template.render())
 
 class RepHandler(webapp2.RequestHandler):
     def get(self):
-        rep_template = jinja_env.get_template("templates/news.html")
+        rep_template = jinja_env.get_template("/templates/representatives.html")
         self.response.write(rep_template.render())
 
 class ServiceHandler(webapp2.RequestHandler):
     def get(self):
-        service_template = jinja_env.get_template("templates/service.html")
+        service_template = jinja_env.get_template("/templates/service.html")
         self.response.write(service_template.render())
 
 app = webapp2.WSGIApplication([
