9b2742fc59418f64a8f263b85ecaa18d6b8ab2d7
Keziah Rezaey <41877887+krezaey@users.noreply.github.com>
2018-08-13 17:13:33 -0700

    Delete news-dict.py

--- news-dict.py
+++
@@ -1,8 +0,0 @@
-import json
-from newsapi import NewsApiClient
-
-newsapi = NewsApiClient(api_key='ebee3df06f3b482084afedd4e3361716')
-
-response = requests.get('https://newsapi.org/v1/articles?apikey=ebee3df06f3b482084afedd4e3361716&sortBy=latest&source=techcrunch')
-
-articles = json.loads(response)
